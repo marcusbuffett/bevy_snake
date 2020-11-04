74bec714ea876842e67af87c7b9c123a218b61b2
Marcus Buffett <marcusbuffett@me.com>
2020-11-04 18:15:20 -0500

    Moving our "snake"

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,18 @@ fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
         .with(SnakeHead);
 }
 
+fn snake_movement(mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        *transform.translation.y_mut() += 2.;
+    }
+}
+
 fn main() {
     App::build()
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup")
         .add_startup_system_to_stage("game_setup", spawn_snake.system())
+        .add_system(snake_movement.system())
         .add_plugins(DefaultPlugins)
         .run();
 }
