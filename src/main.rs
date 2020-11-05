92799430c5871d986c8d6654abb69ff24cb4350c
Marcus Buffett <marcusbuffett@me.com>
2020-11-04 20:55:40 -0500

    Empty window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
+fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn main() {
-    App::build().run();
+    App::build()
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();
 }
