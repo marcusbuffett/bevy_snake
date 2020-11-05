baaefcc8dd046bc86a2fcdf86a4ebe1cb00ee6bc
Marcus Buffett <marcusbuffett@me.com>
2020-11-04 20:56:03 -0500

    Beginnings of a snake

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 #![warn(clippy::complexity)]
 use bevy::prelude::*;
 
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
+}
+
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn(Camera2dComponents::default());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
 }
+
+fn game_setup(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn(SpriteComponents {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .with(SnakeHead);
+}
+
 fn main() {
     App::build()
         .add_startup_system(setup.system())
+        .add_startup_stage("game_setup")
+        .add_startup_system_to_stage("game_setup", game_setup.system())
         .add_plugins(DefaultPlugins)
         .run();
 }
