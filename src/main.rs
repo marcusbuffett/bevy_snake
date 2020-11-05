3f7c30b4d8751c28cfe76d3a5cccbf65b4aa7b29
Marcus Buffett <marcusbuffett@me.com>
2020-11-04 20:55:40 -0500

    Empty window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![warn(clippy::complexity)]
 use bevy::prelude::*;
 
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dComponents::default());
+}
 fn main() {
-    App::build().run();
+    App::build()
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();
 }
