e454fd0476d6ffc764e22e8372fa6250e5d2a3d0
Marcus Buffett <marcusbuffett@me.com>
2021-04-06 13:59:31 -0600

    Beginnings of a snake

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use bevy::prelude::*;
 
-fn setup(mut commands: Commands) {
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
+
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead);
 }
 
 fn main() {
     App::build()
         .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
         .add_plugins(DefaultPlugins)
         .run();
 }
