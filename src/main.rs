b487facea0ca8111ea81003dfedec2f3d591c63f
Marcus <marcusbuffett@me.com>
2022-01-08 15:14:52 -0500

    Beginnings of a snake

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use bevy::prelude::*;
 
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+#[derive(Component)]
+struct SnakeHead;
+
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
 }
 
 fn main() {
     App::new()
         .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
         .add_plugins(DefaultPlugins)
         .run();
 }
