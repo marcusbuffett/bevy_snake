ccdea50c6266a133d89a6f39886b5c427b1629cd
Marcus <marcusbuffett@me.com>
2022-01-08 14:47:20 -0500

    Moving our "snake"

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,17 @@ fn spawn_snake(mut commands: Commands) {
         .insert(SnakeHead);
 }
 
+fn snake_movement(mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        transform.translation.y += 2.;
+    }
+}
+
 fn main() {
     App::new()
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
+        .add_system(snake_movement)
         .add_plugins(DefaultPlugins)
         .run();
 }
