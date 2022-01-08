d7f263735dd8cc7ef5d94b961f1f7554ece780a3
Marcus <marcusbuffett@me.com>
2022-01-08 14:42:01 -0500

    Empty window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
